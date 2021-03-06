6c51039facdc1b099c97fd6be6ccb9705b3c9a9d
codr-mayank <gamblergamer9@gmail.com>
2021-03-06 13:07:07 +0530

    api setup for result screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
+
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  }
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  console.log(result);
 
   return (
     <View>
